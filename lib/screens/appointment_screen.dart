1d128cd1ce0f3f9c723c673dbe4e4a7dd784daef
Hector Illescas <hector.gpillescas@gmail.com>
2025-05-01 20:28:44 -0600

    appointmen_screen(finalizado)

--- lib/screens/appointment_screen.dart
+++ lib/screens/appointment_screen.dart
@@ -23,13 +23,13 @@ class AppointmentScreen extends StatelessWidget {
                 children: [
                   Padding(
                     padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: Stack(
+                    child: Column(
                       children: [
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             InkWell(
-                              onTap: (){
+                              onTap: () {
                                 Navigator.pop(context);
                               },
                               child: Icon(
@@ -39,7 +39,7 @@ class AppointmentScreen extends StatelessWidget {
                               ),
                             ),
                             InkWell(
-                              onTap: (){},
+                              onTap: () {},
                               child: Icon(
                                 Icons.more_vert,
                                 color: Colors.white,
@@ -53,8 +53,9 @@ class AppointmentScreen extends StatelessWidget {
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
-                              CircleAvatar(radius: 35,
-                              backgroundImage: AssetImage("images/doctor1.png"),
+                              CircleAvatar(
+                                radius: 35,
+                                backgroundImage: AssetImage("images/doctor3.png"),
                               ),
                               SizedBox(height: 15),
                               Text(
@@ -107,154 +108,254 @@ class AppointmentScreen extends StatelessWidget {
                             ],
                           ),
                         ),
-                        SizedBox(height: 20),
-                        Container(
-                          height: MediaQuery.of(context).size.height / 1.5,
-                          width: double.infinity,
-                          padding: EdgeInsets.only(
-                            top: 20,
-                            left: 15,
-                          ),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10),
-                              topRight: Radius.circular(10),
-                            ),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              Text(
-                                "About Doctor",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              SizedBox(height: 5),
-                              Text(
-                                "Lore del Doctor, como de que estudio en la UNAM",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black54,
-                                ),
-                              ),
-                              SizedBox(height: 10),
-                              Row(
-                                children: [
-                                  Text(
-                                    "Reviws",
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                  SizedBox(width: 10),
-                                  Icon(Icons.star, color: Colors.amber),
-                                  Text(
-                                    "4.9",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                  SizedBox(width: 5),
-                                  Text(
-                                    "(124)",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 16,
-                                      color: Color(0xFF7165D6),
-                                    ),
-                                  ),
-                                  // spacer aling next widget to the end of row
-                                  Spacer(),
-                                  TextButton(
-                                    onPressed: (){}, 
-                                    child: Text(
-                                      "See all",
-                                        style: TextStyle(
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 16,
-                                        color: Color(0xFF7165D6),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(height: 160,
-                              child: ListView.builder(
-                                scrollDirection: Axis.horizontal,
-                                itemCount: 4,
-                                itemBuilder: (context, index){
-                                return Container(
-                                  margin: EdgeInsets.all(10),
-                                  padding: EdgeInsets.symmetric(vertical: 5),
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(10),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.black12,
-                                        blurRadius: 4,
-                                        spreadRadius: 2,
-                                      )
-                                    ],
-                                  ),
-                                  child: SizedBox(
-                                    width: MediaQuery.of(context).size.width / 1.4,
-                                    child: Column(
-                                      children: [
-                                        ListTile(
-                                          leading: CircleAvatar(
-                                            radius: 25,
-                                            backgroundImage: 
-                                              AssetImage("images/${imgs[index]}"),
-                                          ),
-                                          title: Text(
-                                            "Dr. Doctor Name",
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                          subtitle: Text("1 day ago"),
-                                          trailing: Row(
-                                            mainAxisSize: MainAxisSize.min,
-                                            mainAxisAlignment: MainAxisAlignment.center,
-                                            children: [
-                                              Icon(
-                                                Icons.star,
-                                                color: Colors. amber,
-                                              ),
-                                              Text(
-                                                "4.9",
-                                                style: TextStyle(
-                                                  color: Colors.black54,
-                                                ),
-                                              ),
-                                            ],
-                                          )
-                                        ),
-                                        // 59:32 min
-                                      ],
-                                    ),
-                                  )
-                                );
-                              },),),
-                            ],
-                          ),
-                        ),
                       ],
                     ),
                   ),
                 ],
-              )
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 15,
+                right: 15,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "About Doctor",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "Lorem ipsum is simply dummy text of the printing and\ntypesetting industry.",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Text(
+                        "Reviews",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Icon(Icons.star, color: Colors.amber),
+                      Text(
+                        "4.9",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        "(124)",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                          color: Color(0xFF7165D6),
+                        ),
+                      ),
+                      Spacer(),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "See all",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16,
+                            color: Color(0xFF7165D6),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  SizedBox(
+                    height: 160,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 4,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.all(10),
+                          padding: EdgeInsets.symmetric(vertical: 5),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black12,
+                                blurRadius: 4,
+                                spreadRadius: 2,
+                              )
+                            ],
+                          ),
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width / 1.4,
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  leading: CircleAvatar(
+                                    radius: 25,
+                                    backgroundImage: 
+                                      AssetImage("images/${imgs[index]}"),
+                                  ),
+                                  title: Text(
+                                    "Dr. Doctor Name",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  subtitle: Text("1 day ago"),
+                                  trailing: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        Icons.star,
+                                        color: Colors.amber,
+                                      ),
+                                      Text(
+                                        "4.9",
+                                        style: TextStyle(
+                                          color: Colors.black54,
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ),
+                                SizedBox(height: 5),
+                                Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 10),
+                                  child: Text(
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    "Many thanks to Dr. Dear. He is a great",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Location",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  ListTile(
+                    leading: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF0EEFA),
+                        shape: BoxShape.circle
+                      ),
+                      child: Icon(
+                        Icons.location_on,
+                        color: Color(0xFF7165D6),
+                        size:30,
+                      ),
+                    ),
+                    title: Text(
+                      "New York, Medical Center",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text("address line of the medical center",),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
-      )
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.all(15),
+        height: 130,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 4,
+              spreadRadius: 2,
+            )
+          ],
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Consultation Price",
+                  style: TextStyle(
+                    color: Colors.black54,
+                  ),
+                ),
+                Text(
+                  "\$100",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black54,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 1),
+            InkWell(
+              onTap: (){},
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.symmetric(vertical:18),
+                decoration: BoxDecoration(
+                  color: Color(0xFF7165D6),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    "Book Appointment",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
